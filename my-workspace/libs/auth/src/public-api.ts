2e69ef6b888f6250d642bd79ce185edeac7c8a40
Maxime MEDAL <medal.max@gmail.com>
2024-02-01 16:13:08 +0100

    commit before rooting

---
+++ my-workspace/libs/auth/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of auth
+ */
+
+export * from './lib/auth.service';
+export * from './lib/auth.component';
+export * from './lib/components/login-form/login-form.component';
+
+export * from './lib/models/user'
--- my-workspace/projects/my-app/src/app/app.component.ts
+++ my-workspace/projects/my-app/src/app/app.component.ts
@@ -13,8 +13,8 @@ import { HeaderComponent, MenuComponent, Menu } from 'shared-ui';
 export class AppComponent {
   title = 'my-app';
   menu: Menu = [
-    { title: "page 1", link: 'aa' },
-    { title: "page 2", link: 'aa' },
-    { title: "page 3", link: 'aa' }
+    { title: "page 1", link: '/home' },
+    { title: "page 2", link: '/form' },
+    { title: "page 3", link: '' }
   ];
 }
